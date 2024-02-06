80571b6916844b4f05f1299b3bb28e6924770d3d
Vlad Ciocoiu <vlad.014916@gmail.com>
2024-02-06 14:09:21 +0200

    Add Landing Page + other improvements (#4)
    * create send message input
    * responsive design

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./Home.css";
 
 const Home = () => {
+	const auth = useSelector((state) => state.auth.value);
+
 	return (
-		<div>
-			<h1>Home</h1>
-			<ul>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/register">Register</Link>
-				</li>
-				<li>
-					<Link to="/messages">Messages</Link>
-				</li>
-			</ul>
-		</div>
+		<main className="home-main">
+			<h1>PolyChat</h1>
+			<p>Break barriers effortlessly with our automatic translation chat.</p>
+			<p>Experience seamless communication across languages today!</p>
+			<img
+				className="flags-img"
+				src="images/countryflags.jpg"
+				alt="country flags"
+			/>
+			{!auth.isLoggedIn && (
+				<ul>
+					<li>
+						<button className="login-button">
+							<Link to="/login">Login</Link>
+						</button>
+					</li>
+					<li>
+						<button className="sign-up-button">
+							<Link to="/register">Sign Up</Link>
+						</button>
+					</li>
+				</ul>
+			)}
+		</main>
 	);
 };
 
--- frontend/src/pages/Messages/ChatSection/ChatSection.jsx
+++ frontend/src/pages/Messages/ChatSection/ChatSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { change } from "../../../components/CurrentContact";
 import MessageBubble from "../MessageBubble/MessageBubble.jsx";
 import "./ChatSection.css";
@@ -8,22 +10,57 @@ const ChatSection = () => {
 	const currentContact = useSelector((state) => state.currentContact.value);
 	const dispatch = useDispatch();
 
-	const [messages, setMessages] = React.useState([
-		{
-			id: 1,
-			timestamp: "2021-01-01T00:00:00.000Z",
-			senderId: 1,
-			receiverId: 0,
-			text: `Hi, my name is ${currentContact?.name}!`,
-		},
-		{
-			id: 2,
-			timestamp: "2023-02-01T00:00:00.000Z",
+	const [messages, setMessages] = useState([]);
+	const [messageText, setMessageText] = useState("");
+	const scrollableRef = useRef();
+
+	useEffect(() => {
+		setMessages([
+			{
+				id: 1,
+				timestamp: "2021-01-01T00:00:00.000Z",
+				senderId: 1,
+				receiverId: 0,
+				text: `Hi, my name is ${currentContact?.name}!`,
+			},
+			{
+				id: 2,
+				timestamp: "2023-02-01T00:00:00.000Z",
+				senderId: 0,
+				receiverId: 1,
+				text: "Hello, how are you?",
+			},
+		]);
+	}, [currentContact]);
+
+	const scrollToBottom = () => {
+		if (scrollableRef.current) {
+			scrollableRef.current.scrollTop = scrollableRef.current.scrollHeight;
+		}
+	};
+
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
+
+	const handleMessageChange = (e) => {
+		setMessageText(e.target.value);
+	};
+
+	const handleSendMessage = (e) => {
+		e.preventDefault();
+		if (!messageText) return;
+
+		const newMessage = {
+			id: messages.length + 1,
+			timestamp: new Date().toISOString(),
 			senderId: 0,
 			receiverId: 1,
-			text: "Hello, how are you?",
-		},
-	]);
+			text: messageText,
+		};
+		setMessages([...messages, newMessage]); // here there should be a request to the server
+		setMessageText("");
+	};
 
 	return (
 		<div className="chat-section">
@@ -31,11 +68,22 @@ const ChatSection = () => {
 				<p className="contact-name">{currentContact?.name}</p>
 				<p className="contact-email">{currentContact?.email}</p>
 			</div>
-			<div className="message-list">
+			<div className="message-list" ref={scrollableRef}>
 				{messages.map((message, index) => (
 					<MessageBubble key={index} message={message} />
 				))}
 			</div>
+			<form className="message-input" onSubmit={handleSendMessage}>
+				<input
+					type="text"
+					placeholder="Type a message..."
+					value={messageText}
+					onChange={handleMessageChange}
+				/>
+				<button type="submit">
+					<FontAwesomeIcon icon={faPaperPlane} />
+				</button>
+			</form>
 		</div>
 	);
 };
