2adc07ca344bf2f25860bb3380f7534b52cfc877
Vlad Ciocoiu <vlad.014916@gmail.com>
2024-02-07 12:07:21 +0200

    Add profile pictures and display contact card

--- frontend/src/pages/Messages/ContactSection/ContactSection.jsx
+++ frontend/src/pages/Messages/ContactSection/ContactSection.jsx
@@ -1,29 +1,101 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./ContactSection.css";
 import { change } from "../../../components/CurrentContact";
+import ImageComponent from "./ImageComponent";
 
-const ContactSection = ({ isOpen }) => {
+const ContactSection = ({ isOpen, setCardIsOpen }) => {
 	const currentContact = useSelector((state) => state.currentContact.value);
 	const dispatch = useDispatch();
 
 	const mockContacts = [
-		{ id: 1, name: "John Doe", email: "john.doe@gmail.com" },
-		{ id: 2, name: "Jane Doe", email: "jane.doe@gmail.com" },
-		{ id: 3, name: "John Smith", email: "john.smith@gmail.com" },
-		{ id: 4, name: "Jane Smith", email: "jane.smith@gmail.com" },
-		{ id: 5, name: "John Doe", email: "john.doe@gmail.com" },
-		{ id: 6, name: "John Doe 2", email: "john.doe2@gmail.com" },
-		{ id: 7, name: "Jane Doe 2", email: "jane.doe2@gmail.com" },
-		{ id: 8, name: "John Smith 2", email: "john.smith2@gmail.com" },
-		{ id: 9, name: "Jane Smith 2", email: "jane.smith2@gmail.com" },
-		{ id: 10, name: "John Doe 2", email: "john.doe2@gmail.com" },
+		{ id: 1, name: "John Doe", email: "john.doe@gmail.com", profilePicture: "" },
+		{ id: 2, name: "Jane Doe", email: "jane.doe@gmail.com", profilePicture: "" },
+		{
+			id: 3,
+			name: "John Smith",
+			email: "john.smith@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 4,
+			name: "Jane Smith",
+			email: "jane.smith@gmail.com",
+			profilePicture: "",
+		},
+		{ id: 5, name: "John Doe", email: "john.doe@gmail.com", profilePicture: "" },
+		{
+			id: 6,
+			name: "John Doe 2",
+			email: "john.doe2@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 7,
+			name: "Jane Doe 2",
+			email: "jane.doe2@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 8,
+			name: "John Smith 2",
+			email: "john.smith2@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 9,
+			name: "Jane Smith 2",
+			email: "jane.smith2@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 10,
+			name: "John Doe 2",
+			email: "john.doe2@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 11,
+			name: "John Doe 3",
+			email: "john.doe3@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 12,
+			name: "Jane Doe 3",
+			email: "jane.doe3@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 13,
+			name: "John Smith 3",
+			email: "john.smith3@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 14,
+			name: "Jane Smith 3",
+			email: "jane.smith3@gmail.com",
+			profilePicture: "",
+		},
+		{
+			id: 15,
+			name: "John Doe 3",
+			email: "john.doe3@gmail.com",
+			profilePicture: "",
+		},
 	];
 	const [contacts, setContacts] = useState(mockContacts);
 
+	useEffect(() => {
+		dispatch(change(contacts[0]));
+	}, []);
+
 	return (
 		<div className={"contact-section " + (isOpen ? "open" : "closed")}>
-			<h1>Contacts</h1>
+			<div className="contacts-heading-div">
+				<h1>Contacts</h1>
+			</div>
 			<ul className="contact-list">
 				{contacts.map((contact, index) => (
 					<li
@@ -33,6 +105,13 @@ const ContactSection = ({ isOpen }) => {
 						}
 						onClick={() => dispatch(change(contact))}
 					>
+						<ImageComponent
+							image={
+								contact.profilePicture || "public/images/default-profile-picture.jpg"
+							}
+							contact={contact}
+							setCardIsOpen={setCardIsOpen}
+						/>
 						<p className="contact-name">{contact.name}</p>
 					</li>
 				))}
--- frontend/src/pages/Messages/Messages.jsx
+++ frontend/src/pages/Messages/Messages.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ChatSection from "./ChatSection/ChatSection.jsx";
 import ContactSection from "./ContactSection/ContactSection.jsx";
+import ContactCard from "./ContactCard/ContactCard.jsx";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
@@ -8,19 +9,27 @@ import "./Messages.css";
 
 const Messages = () => {
 	const [isContactSectionOpen, setIsContactSectionOpen] = useState(true);
+	const [cardIsOpen, setCardIsOpen] = useState(false);
+
+	const toggleContactSection = () => {
+		if (cardIsOpen) setCardIsOpen(false);
+		setIsContactSectionOpen((open) => !open);
+	};
 
 	return (
 		<div className="messages-page">
-			<ContactSection isOpen={isContactSectionOpen} />
+			<ContactSection
+				isOpen={isContactSectionOpen}
+				setCardIsOpen={setCardIsOpen}
+			/>
 			<ChatSection />
+			{cardIsOpen && <ContactCard setCardIsOpen={setCardIsOpen} />}
 			<button
 				className={
 					"contact-section-toggle icon-button" +
 					(isContactSectionOpen ? " open" : " closed")
 				}
-				onClick={() =>
-					setIsContactSectionOpen((isContactSectionOpen) => !isContactSectionOpen)
-				}
+				onClick={toggleContactSection}
 			>
 				<FontAwesomeIcon icon={isContactSectionOpen ? faArrowLeft : faUser} />
 			</button>
