40d7894b90020d1be7ee6166e714d345f4c47862
Vlad Ciocoiu <vlad.014916@gmail.com>
2024-03-22 14:08:26 +0200

    Finish auth system

--- backend/src/middleware/validateForm.js
+++ backend/src/middleware/validateForm.js
@@ -20,7 +20,7 @@ const loginSchema = Joi.object({
 exports.validateLogin = (req, res, next) => {
 	const { error } = loginSchema.validate(req.body);
 
-	if (error) return res.status(400).json({ error: error.details[0].message });
+	if (error) return res.status(400).json({ error: "Invalid credentials." });
 
 	next();
 };
